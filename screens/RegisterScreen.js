c2daa974fa1c96abc97a18c57e9485cf13aa25a2
ankitksh81 <ankitksh81@gmail.com>
2021-01-29 00:25:58 +0530

    input fields added for register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
+import { KeyboardAvoidingView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { Input } from "react-native-elements";
 
 const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const register = () => {};
+
   return (
-    <View>
-      <Text>This is Register Screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create a Signal account
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full Name"
+          autoFocus
+          type="text"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+
+        <Input
+          placeholder="Password"
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+
+        <Input
+          placeholder="Profile Picture URL (optional)"
+          type="text"
+          value={imageUrl}
+          onChangeText={(text) => setImage(text)}
+          onSubmitEditing={register}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 

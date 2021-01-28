cf6a846416ba1ab73a95d60206a8b2d1a136ae23
ankitksh81 <ankitksh81@gmail.com>
2021-01-28 23:59:02 +0530

    added logo, inputs, buttons without fn

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Button, Input, Image } from "react-native-elements";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = {};
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  inputContainer: {},
+  button: {},
+});
